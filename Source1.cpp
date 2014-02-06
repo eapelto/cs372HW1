984e68b3ae3f7ce280bf53617e8ac572225bcfeb
Eric Pelto <eapelto@alaska.edu>
2014-02-05 23:42:57 -0900

    I'm am using the unstage function.

--- Source1.cpp
+++ Source1.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
+using std::cout;
+using std::endl;
 
 int main()
 {
 	cout << "Working with git" << endl;
-
+	cout << "Working with git need to become more complicated." << endl;
 	return 0;
 }
